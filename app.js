7b1f28d7171215aa49e44c0d2058dec51547d008
franky3553 <franky3553@gmail.com>
2022-07-05 23:46:02 +0200

    Add toggle event

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const toggleBtn = document.querySelector('.sidebar-toggle');
 const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
 
+// toggle event
+toggleBtn.addEventListener('click', function(){
+   sidebar.classList.toggle('show-sidebar');
+});

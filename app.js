cdca792ebf2297957d73b124ec7e0b25676ba475
franky3553 <franky3553@gmail.com>
2022-07-05 23:47:54 +0200

    Add close sidebar event

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const toggleBtn = document.querySelector('.sidebar-toggle');
 const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
 
-// toggle event
+// toggle sidebar event
 toggleBtn.addEventListener('click', function(){
    sidebar.classList.toggle('show-sidebar');
+});
+
+// close sidebar event
+closeBtn.addEventListener('click', function(){
+   sidebar.classList.remove('show-sidebar')
 });
